222866a5f0934cb6723fa963cec381d6feacf179
AgentYarik <125293714+AgentYarik@users.noreply.github.com>
2023-09-29 17:39:42 +0300

    dfdfd

---
+++ script.js
@@ -0,0 +1,85 @@
+
+/* const celsises = 25;
+
+
+const fahrenрeitе = (celsises* 9/5) + 32;
+
+console.log(celsises + "C");
+console.log(fahrenрeitе + "f"); */
+
+
+/* const dni = 28; 
+
+
+const chas = 24;
+
+
+const minutas = 60;
+
+
+const vidpovidy = dni * chas;
+
+
+const vidpodid = chas * minutas;
+
+
+console.log(chas);
+console.log(minutas);
+console.log(dni) */
+
+
+/* var health = 100; 
+var energy = 50;  
+
+
+health = health - 20; 
+energy = energy - 10; 
+
+
+console.log("Здоровя" + health);
+console.log("Енергія" + energy); */
+
+/* const unicornHugs = 1000;
+const rainbowDiscount = 0.2; 
+
+const enchantedTotal = unicornHugs * (1 - rainbowDiscount);
+
+console.log(enchantedTotal); */
+
+/* const chiclo = 5.9; 
+
+const chicloTop = Math.floor(chiclo); 
+
+console.log(chicloTop); */
+
+
+/* let numberik = "3.22"; 
+
+let superMario = parseFloat(numberik); 
+
+console.log(superMario); */
+
+/* const numberPochka = "6.2"; 
+
+const pochicka = parseInt(numberPochka); 
+
+console.log(pochicka); */
+
+/* const vceraDnuha = 24; 
+
+const vcheraDnuha = Math.sqrt(vceraDnuha); 
+
+console.log(vcheraDnuha); */
+
+/* const numberikr = 36; 
+const notionNumber = "14";
+
+
+const yarikNumber = parseInt(notionNumber);
+
+console.log(yarikNumber);
+
+
+const numberYarik = numberikr.toString();
+
+console.log(notionNumber); */
